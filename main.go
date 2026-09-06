b5641c149a53a7d4a789d050c8b4ed827c364cce
GeorgeHat <georgehat2003@gmail.com>
2026-09-06 13:22:59 +0300

    Added a message validation handler for the api

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 	"sync/atomic"
 )
 
@@ -14,8 +16,9 @@ func main() {
 	appHandler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
 	serveMux.Handle("/app/", cfg.middlewareMetricsInc(appHandler))
 	serveMux.HandleFunc("GET /api/healthz", healthzHandlerFunc)
-	serveMux.HandleFunc("GET /api/metrics", cfg.getMetricsHandler)
-	serveMux.HandleFunc("POST /api/reset", cfg.resetMetricsHandler)
+	serveMux.HandleFunc("GET /admin/metrics", cfg.getMetricsHandler)
+	serveMux.HandleFunc("POST /admin/reset", cfg.resetMetricsHandler)
+	serveMux.HandleFunc("POST /api/validate_chirp", validateChirpHandler)
 	server := new(http.Server)
 	server.Addr = ":8080"
 	server.Handler = serveMux
@@ -42,10 +45,68 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) getMetricsHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hits: %d", cfg.fileserverHits.Load())
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprintf(w,
+		`<html>
+			<body>
+				<h1>Welcome, Chirpy Admin</h1>
+				<p>Chirpy has been visited %d times!</p>
+			</body>
+		</html>`,
+		cfg.fileserverHits.Load())
 }
 
 func (cfg *apiConfig) resetMetricsHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hits reset to 0")
 	cfg.fileserverHits.Store(0)
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) error {
+	return respondWithJSON(w, code, map[string]string{"error": msg})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) error {
+	data, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(data)
+	return nil
+}
+
+func validateChirpHandler(w http.ResponseWriter, r *http.Request) {
+	type requestParams struct {
+		Body string `json:"body"`
+	}
+	decoder := json.NewDecoder(r.Body)
+	params := &requestParams{}
+	err := decoder.Decode(params)
+	if err != nil {
+		fmt.Println("Error decoding request json body")
+		return
+	}
+	type responseParams struct {
+		CleanedBody string `json:"cleaned_body"`
+	}
+	if len(params.Body) > 140 {
+		respondWithError(w, 400, "Chirp is too long")
+		return
+	}
+	response := responseParams{CleanedBody: cleanedInput(params.Body)}
+	respondWithJSON(w, 200, response)
+
+}
+
+func cleanedInput(input string) string {
+	banned := map[string]bool{"kerfuffle": true, "sharbert": true, "fornax": true}
+	words := strings.Split(input, " ")
+	for i, word := range words {
+		if banned[strings.ToLower(word)] {
+			words[i] = "****"
+		}
+	}
+	output := strings.Join(words, " ")
+	return output
 }
